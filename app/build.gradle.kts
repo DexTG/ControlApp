d5572e10994c5143d3a2c12592a9d3c19e9a311e
DexTG <86502511+DexTG@users.noreply.github.com>
2025-09-18 09:12:05 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,8 @@ android {
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.09.02")
     implementation(composeBom); androidTestImplementation(composeBom)
-    
+
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.2")
